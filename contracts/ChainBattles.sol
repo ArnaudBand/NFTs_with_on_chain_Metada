34ccb712593eda0beb8c988f153669fdd945bbda
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-01-26 22:45:28 +0300

    Create the Train Function to raise your NFT Level

--- contracts/ChainBattles.sol
+++ contracts/ChainBattles.sol
@@ -16,23 +16,28 @@ contract ChainBattles is ERC721URIStorage {
 
     constructor() ERC721("Chain Battles", "CBTLS") {}
 
-    function generateCharacter(uint256 tokenId) public returns(string memory) {
-
-    bytes memory svg = abi.encodePacked(
-        '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
-          '<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
-          '<rect width="100%" height="100%" fill="black" />',
-          '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',"Warrior",'</text>',
-          '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Levels: ",getLevels(tokenId),'</text>',
-        '</svg>'
-    );
-    return string(
-        abi.encodePacked(
-            "data:image/svg+xml;base64,",
-            Base64.encode(svg)
-        )    
-    );
-}
+    function generateCharacter(uint256 tokenId) public returns (string memory) {
+        bytes memory svg = abi.encodePacked(
+            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+            "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
+            '<rect width="100%" height="100%" fill="black" />',
+            '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Warrior",
+            "</text>",
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Levels: ",
+            getLevels(tokenId),
+            "</text>",
+            "</svg>"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:image/svg+xml;base64,",
+                    Base64.encode(svg)
+                )
+            );
+    }
 
     function getLevels(uint256 tokenId) public view returns (string memory) {
         uint256 levels = tokenIdtoLevels[tokenId];
@@ -66,5 +71,16 @@ contract ChainBattles is ERC721URIStorage {
         _safeMint(msg.sender, newItemId);
         tokenIdtoLevels[newItemId] = 0;
         _setTokenURI(newItemId, getTokenURI(newItemId));
+    }
+
+    function train(uint256 tokenId) public {
+        require(_exists(tokenId), "Please use an existing token");
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "You must own this token to train it"
+        );
+        uint256 currentLevel = tokenIdtoLevels[tokenId];
+        tokenIdtoLevels[tokenId] = currentLevel + 1;
+        _setTokenURI(tokenId, getTokenURI(tokenId));
     }
 }
