14529fc239d9363d8230a1cc4d5f364a907dc925
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-01-27 01:21:01 +0300

    Add the key need for the deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apikey: process.env.POLYGONSCAN_API_KEY
+  }
 };
